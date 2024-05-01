af651c504fe161f80aab5c776f89cf260bf0205b
bridgetegan3 <began3@wisc.edu>
2024-04-30 20:02:02 -0500

    Added splash

---
+++ Assets/Paintball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paintballeffect : MonoBehaviour
+{
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+
+        }
+        // if (collision.relativeVelocity.magnitude > 2)
+        audioSource.Play();
+    }
+
+
+}
